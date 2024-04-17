65b6333f675ea5ad53f1a060858c5a4c97aac4a7
Arturo Chica Villa <arturochicavilla@hotmail.com>
2024-04-17 09:38:29 +0200

    upload pictures users

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -5,22 +5,23 @@ const nodemailer = require("nodemailer");
 
 const register = (req, res) => {
 
-    const q = "SELECT * FROM users WHERE email=? OR username = ?"
-    db.query(q,[req.body.email, req.body.username], (err,data)=>{
+    const q1 = "SELECT * FROM users WHERE email=? OR username = ?"
+    db.query(q1,[req.body.email, req.body.username], (err,data)=>{
         if(err) return res.json(err)
         if(data.length) return res.status(409).json("User already exists")
 
         const salt = bcrypt.genSaltSync(10)
         const hash = bcrypt.hashSync(req.body.password, salt)
 
-        const q = "INSERT INTO users(`username`, `email`, `password`) VALUES (?)"
+        const q2 = "INSERT INTO users(`username`, `email`, `password`, `img`) VALUES (?, ?, ?, ?)"
         const values = [
             req.body.username,
             req.body.email,
-            hash
+            hash,
+            req.body.img ? req.body.img.path : null
         ]
 
-        db.query(q, [values], (err,data)=> {
+        db.query(q2, [values], (err,data)=> {
             if(err) return res.json(err)
             return res.status(200).json("User has been created successfully")
         })
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,43 +1,54 @@
-import pe from "../images/login/pe.jpg"
-import { useState } from "react"
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
     const [inputs, setInputs] = useState({
-        username:"",
-        email:"",
-        password:"",
-    })
+        username: "",
+        email: "",
+        password: "",
+        img: null
+    });
 
-    const [error, setError] = useState(null)
-
-    const navigate = useNavigate()
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
 
     const handleChange = (e) => {
-        setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
-    }
-
-    const handleSubmit = async e =>{
-        e.preventDefault()
-        try{
-        const res = await axios.post("https://proyecto-escrache.onrender.com/api/auth/register", inputs)
-            navigate("/login")
-        }catch(err){
-            setError(err.response.data)
+        if (e.target.name === "img") {
+            setInputs(prev => ({ ...prev, [e.target.name]: e.target.files[0] }));
+        } else {
+            setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }));
         }
-    }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const formData = new FormData();
+            formData.append("username", inputs.username);
+            formData.append("email", inputs.email);
+            formData.append("password", inputs.password);
+            formData.append("img", inputs.img); 
+            const res = await axios.post("https://proyecto-escrache.onrender.com/api/auth/register", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            });
+            navigate("/login");
+        } catch (err) {
+            setError(err.response.data);
+        }
+    };
 
     return (
-<       div className="signup-container">
+        <div className="signup-container">
             <form className="signupinfo-container">
-                <img src={pe} alt="log in" />
-                <input type="text" placeholder="Enter username" required name="username" onChange={handleChange}/>
-                <input type="email" placeholder="Enter email" required name="email" onChange={handleChange}/>
-                <input type="password" placeholder="Enter password" required name="password" onChange={handleChange}/>
+                <input type="text" placeholder="Enter username" required name="username" onChange={handleChange} />
+                <input type="email" placeholder="Enter email" required name="email" onChange={handleChange} />
+                <input type="password" placeholder="Enter password" required name="password" onChange={handleChange} />
+                <input type="file" accept="img/*" required name="img" onChange={handleChange} />
                 <button onClick={handleSubmit} type="submit">Create a new account</button>
                 {error && <p>{error}</p>}
-
             </form>
         </div>
     );
